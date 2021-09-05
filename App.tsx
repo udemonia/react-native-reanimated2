7760ef2b6bc5b3ab89b7b953292dcfbf832d5c49
udemonia <brandon.l.lambert@icloud.com>
2021-09-05 17:37:15 -0500

    updated with spring

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import Animated, {
   withTiming,
   useAnimatedStyle,
   Easing,
+  withSpring,
 } from 'react-native-reanimated';
 
 //* Taken from the below video series
@@ -17,6 +18,7 @@ export default function App() {
 
   //* use shared value - create a value that can be handled from the ui thread
   const progress = useSharedValue(1)
+  const scale = useSharedValue(1)
 
   //* use animated style - similar to the stylesheet styles
   // https://docs.swmansion.com/react-native-reanimated/docs/api/useAnimatedStyle/
@@ -24,16 +26,19 @@ export default function App() {
   //
   const reanimatedStyle = useAnimatedStyle(() => {
     return {
-      opacity: progress.value
+      opacity: progress.value,
+      borderRadius: (progress.value * SIZE) / 2,
+      transform: [{ scale: scale.value }]
 
     }
   }, []) 
 
   useEffect(() => {
-    progress.value = withTiming(0)
-    // return () => {
-    //   cleanup
-    // }
+
+    //* withTiming to value + clock??
+    progress.value = withSpring(0.5)
+    scale.value = withSpring(1)
+
   }, [])
 
   return (
